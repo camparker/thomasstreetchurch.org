0eada352469b162acfd203ca0883bd5725db2907
Cameron Parker <ctrlaltdelete63@gmail.com>
2020-02-08 10:48:09 -0500

    Added font awesome library and edited footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,35 @@
 import React from "react"
+import { Link } from "gatsby"
 import Theme from "./theme"
 import styled from "styled-components"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCopyright } from "@fortawesome/free-solid-svg-icons"
+
 const StyledFooter = styled.footer`
   width: 100%;
+  text-align: center;
+  font-size: 10px;
   background-color: ${props => props.theme.colors.lightPurple};
+  color: ${props => props.theme.colors.darkPurple};
 `
+const StyledLink = styled(Link)`
+  display: inline-block;
+  text-decoration: none;
+  color: ${props => props.theme.colors.darkPurple};
+
+  &:hover {
+    color: ${props => props.theme.colors.eggshellWhite};
+  }
+`
+
 const Footer = () => (
   <Theme>
     <StyledFooter>
-      <h1>Thomas Street Church Of Christ</h1>
+      <p>
+        <FontAwesomeIcon icon={faCopyright} /> {new Date().getFullYear()} |{" "}
+        <StyledLink to="/news">Thomas Street Church of Christ</StyledLink>
+      </p>
     </StyledFooter>
   </Theme>
 )
