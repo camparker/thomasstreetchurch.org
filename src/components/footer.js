b7298695941781f73d65d92eefc37062fbce9ab4
Cameron Parker <ctrlaltdelete63@gmail.com>
2020-02-08 11:02:40 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,32 @@
 import React from "react"
+import { Link } from "gatsby"
 import Theme from "./theme"
 import styled from "styled-components"
 
 const StyledFooter = styled.footer`
   width: 100%;
-  background-color: ${props => props.theme.colors.lightPurple};
+  text-align: center;
+  font-size: 10px;
+  background-color: ${props => props.theme.colors.midPurple};
+  color: ${props => props.theme.colors.eggshellWhite};
 `
+const StyledLink = styled(Link)`
+  display: inline-block;
+  text-decoration: none;
+  color: ${props => props.theme.colors.eggshellWhite};
+
+  &:hover {
+    color: ${props => props.theme.colors.darkPurple};
+  }
+`
+
 const Footer = () => (
   <Theme>
     <StyledFooter>
-      <h1>Thomas Street Church Of Christ</h1>
+      <p>
+        Copyright {new Date().getFullYear()} |{" "}
+        <StyledLink to="/">Thomas Street Church of Christ</StyledLink>
+      </p>
     </StyledFooter>
   </Theme>
 )
