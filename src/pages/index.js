fd999426d92e252465e8704799994add7463a74d
Cameron Parker <ctrlaltdelete63@gmail.com>
2020-02-08 12:40:31 -0500

    Added image to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,66 @@
 import React from "react"
-import { Link } from "gatsby"
 import { graphql } from "gatsby"
+import Theme from "../components/theme"
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IndexImage from "../components/index-image"
+
+const Jumbotron = styled.div`
+  position: relative;
+  text-align: center;
+  color: ${props => props.theme.colors.eggshellWhite};
+  @media (min-width: 768px) {
+  }
+`
+
+const JumbotronImage = styled.div`
+  width: 100%;
+
+  @media (min-width: 768px) {
+  }
+`
+
+const JumbotronText = styled.div`
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  @media (max-width: 768px) {
+    top: 50%;
+  }
+`
+const Title = styled.p`
+  border-bottom: 2px solid ${props => props.theme.colors.lightYellow};
+  font-size: ${props => props.theme.fontSizes.medium};
+`
+
+const Subtitle = styled.p`
+  display: inline-block;
+  margin: 0;
+  border-bottom: 2px solid ${props => props.theme.colors.lightYellow};
+  font-size: ${props => props.theme.fontSizes.small};
+`
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>{data.markdownRemark.frontmatter.title}</h1>
-    <p>{data.markdownRemark.frontmatter.subtitle}</p>
-    <p>{data.markdownRemark.frontmatter.date}</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <Theme>
+    <Layout>
+      <SEO title="Home" />
+      <Jumbotron>
+        <JumbotronImage>
+          <IndexImage />
+        </JumbotronImage>
+        <JumbotronText>
+          <Title>
+            <p>{data.markdownRemark.frontmatter.title}</p>
+          </Title>
+          <Subtitle>
+            <p>{data.markdownRemark.frontmatter.subtitle}</p>
+          </Subtitle>
+        </JumbotronText>
+      </Jumbotron>
+    </Layout>
+  </Theme>
 )
 
 export default IndexPage
